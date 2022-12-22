a4ebfc24108f2b13f55ab3a39d2b952a42317f85
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2022-12-22 09:52:16 -0800

    startup

---
+++ Startup.sh
@@ -0,0 +1,21 @@
+
+cd ~
+
+g++ /home/pi/RaspberryPiSensorCode/rpi/lidar.cpp /home/pi/RaspberryPiSensorCode/rpi/src/lidarlite_v3.cpp -I . -o /home/pi/RaspberryPiSensorCode/bin/lidar.out
+g++ -Wall -pthread /home/pi/RaspberryPiSensorCode/rpi/ultrasonic.cpp -I . -lpigpio -lrt -o /home/pi/RaspberryPiSensorCode/bin/us.out
+
+./home/pi/RaspberryPiSensorCode/bin/lidar.out
+
+echo "[Unit]
+Description=SensorUDP
+After=multi-user.target
+[Service]
+Type=idle
+ExecStart=./home/pi/RaspberryPiSensorCode/bin/lidar.out
+[Install]
+WantedBy=multi-user.target" | sudo tee /lib/systemd/system/sensorsUDP.service
+
+
+sudo chmod 644 /lib/systemd/system/sensorsUDP.service
+sudo systemctl daemon-reload
+sudo systemctl enable sensorsUDP.service
