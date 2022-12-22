f81688a50d855cf21ab814c2ec6ce97ba963eb3c
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2022-12-22 10:29:09 -0800

    fix

--- Run.sh
+++ Run.sh
@@ -3,4 +3,4 @@ cd ~
 cd /home/pi/RaspberryPiSensorCode/
 g++ /home/pi/RaspberryPiSensorCode/rpi/lidar.cpp /home/pi/RaspberryPiSensorCode/rpi/src/lidarlite_v3.cpp -I . -o /home/pi/RaspberryPiSensorCode/bin/lidar.out
 g++ -Wall -pthread /home/pi/RaspberryPiSensorCode/rpi/ultrasonic.cpp -I . -lpigpio -lrt -o /home/pi/RaspberryPiSensorCode/bin/us.out
-sudo ./home/pi/RaspberryPiSensorCode/bin/lidar.out
+./home/pi/RaspberryPiSensorCode/bin/lidar.out
