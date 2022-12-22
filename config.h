af8ee2613f556b4b986ab732ac13a2f01cf25774
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2022-12-22 09:31:00 -0800

    infostuffs

--- config.h
+++ config.h
@@ -1,4 +1,16 @@
 #include <string>
 
 std::string ID = "0:";
-int lidar_mode = 4;
+int lidar_mode = 2;
+/*
+    0: Default mode, balanced performance.
+    1: Short range, high speed. Uses 0x1d maximum acquisition count.
+    2: Default range, higher speed short range. Turns on quick termination
+        detection for faster measurements at short range (with decreased
+        accuracy)
+    3: Maximum range. Uses 0xff maximum acquisition count.
+    4: High sensitivity detection. Overrides default valid measurement detection
+        algorithm, and uses a threshold value for high sensitivity and noise.
+    5: Low sensitivity detection. Overrides default valid measurement detection
+        algorithm, and uses a threshold value for low sensitivity and noise.
+/*
