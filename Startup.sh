f61fd91c76010193d7077c4aa66d6b24580f36cc
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2022-12-22 09:56:07 -0800

    fix

--- Startup.sh
+++ Startup.sh
@@ -5,8 +5,7 @@ cd /home/pi/RaspberryPiSensorCode/
 g++ /home/pi/RaspberryPiSensorCode/rpi/lidar.cpp /home/pi/RaspberryPiSensorCode/rpi/src/lidarlite_v3.cpp -I . -o /home/pi/RaspberryPiSensorCode/bin/lidar.out
 g++ -Wall -pthread /home/pi/RaspberryPiSensorCode/rpi/ultrasonic.cpp -I . -lpigpio -lrt -o /home/pi/RaspberryPiSensorCode/bin/us.out
 
-cd
-./home/pi/RaspberryPiSensorCode/bin/lidar.out
+cd ~
 
 echo "[Unit]
 Description=SensorUDP
