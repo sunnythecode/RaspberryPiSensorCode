fd125566b1b2e1136dc1c6741d82ab79246b89f9
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2022-12-22 08:16:27 -0800

    bruh2

--- rpi/src/lidarlite_v3.cpp
+++ rpi/src/lidarlite_v3.cpp
@@ -24,7 +24,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#include "include/lidarlite_v3.h"
+#include "rpi/include/lidarlite_v3.h"
 
 /*------------------------------------------------------------------------------
   I2C Init
